350f85e2d65e58cc53c84c8446340891a0e7a00a
Stephen-Cook341 <stephenmarkcook@hotmail.co.uk>
2022-03-07 13:03:40 +0000

    added basic netcode

--- cardioglist-terminal.py
+++ cardioglist-terminal.py
@@ -1,14 +1,13 @@
-import pwd
-import random
 import sys
-import bcrypt
 from tools import whitelist_input,check_user
-
+import socket
+import errno
+import threading
+from getpass import getpass
 
 #Shared key encryption scheme and Diffiehelman
 
-#TODO Stretch goal: Connect to db 
-#TODO Strecth Goal: Store user credentials and pacemaker history
+#TODO Stretch Goal: Store user credentials and pacemaker history
 
 #diffelman, Encrypt and decrypt on Pacemaker and client 
 
@@ -22,12 +21,57 @@ class Cardio_client():
         while self.attempts > 0:
     
             username = whitelist_input(input("Enter Username: ")) 
-            password = whitelist_input(input("Enter Password: ")) 
+            password = whitelist_input(getpass(prompt= 'Enter Password: ', mask='*')) 
         
-        #print(username," ", password)
-            self.verify_credentials(username,password)
+        print(username," ", password)
+            #self.verify_credentials(username,password)
+        self.create_server()
+        
+    def create_server(self):
+        #tcp socket
+        self._ip = '127.0.0.1'
+        self._port = 8080
+        self._buff_size = 2048
+        
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock.bind((self._ip,self._port))
+        self.sock.listen(1)
+        self.connections =[]
+        
+        self.client_listener()
+        
+        
+    #handles client connections
+    def client_handler(self,c,a):
+        while True:
+            data = c.recv(self._buff_size)
+            print(data)
+            #for connection in self.connections:
+                #connection.send(data)
+            if not data:
+                break
             
-    
+    def decode_message(self):
+        pass
+        #decrypt
+        
+            
+    def client_listener(self):
+        
+        while True:
+            
+            c,a = self.sock.accept()
+            client_thread = threading.Thread(target=self.client_handler,args=(c,a))
+            client_thread.daemon = True
+            client_thread.start()
+            self.connections.append(c)
+            print(self.connections)
+            
+     
+            
+        
+        
+
         
     def verify_credentials(self,username,password):
         
--- pacemaker.py
+++ pacemaker.py
@@ -1,4 +1,5 @@
-import random
+import socket
+import json as js
 
 class Pacemaker_model():
     
@@ -8,9 +9,10 @@ class Pacemaker_model():
         self.lower_threshold = 0 
         self.higher_threshold = 0
         self.battery_capacity = 3500
-        self.battery_current_charge 
+        #self.battery_current_charge 
         self.decrypt = True
         
+        self.client_net_code()
         
         if self.decrypt == True:
             self.decrypt_command()
@@ -21,4 +23,27 @@ class Pacemaker_model():
         
             
     def edit_settings():
-         
+         pass
+     
+    def client_net_code(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+        
+        self.ip = '127.0.0.1'
+        self.port = 8080
+        self.sock.connect((self.ip,self.port))
+        self.send_connect_msg()
+        
+     
+    def send_connect_msg(self):
+        pacemaker_id = 5
+        
+        msg = {"data":[
+                {"header":"pacemaker_identity",
+                        "pacemaker_id":pacemaker_id}
+                ]
+            }
+        data = "sent data"
+        
+        self.sock.send(data.encode())
+   
+dummy_pacemaker = Pacemaker_model()
